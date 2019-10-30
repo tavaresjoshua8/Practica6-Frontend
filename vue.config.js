ce6f6c301a1acf8e724bf5570ea6f80527e313ea
Joshua Salcido <tavaresjoshua8@gmail.com>
2019-10-30 09:01:03 -0700

    modified:   vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	"devServer": {
-		"proxy": "http://laracon.test"
+		"proxy": "http://localhost:8080"
 	},
 	"outputDir": "../public",
 	indexPath: process.env.NODE_ENV === 'production'
-    ? '../resources/views/index.blade.php'
-    : 'index.html',
+        ? '../resources/views/index.blade.php'
+        : 'index.html',
 	"transpileDependencies": [
 		"vuetify"
 	]
